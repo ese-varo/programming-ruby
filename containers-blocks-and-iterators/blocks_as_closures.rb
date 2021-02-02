cedf95812e9937df38fad80f32fb75c3865a1a6c
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-02 11:07:32 -0600

    Add alternative notation for proc objects

--- containers-blocks-and-iterators/blocks_as_closures.rb
+++ containers-blocks-and-iterators/blocks_as_closures.rb
@@ -18,3 +18,13 @@ power_proc = power_proc_generator
 puts power_proc.call
 puts power_proc.call
 puts power_proc.call
+
+# An alternative notation for creating block objects 
+
+proc1 = -> arg { puts "In proc1 with #{arg}" }
+proc2 = -> arg1, arg2 { puts "In proc2 with #{arg1} and #{arg2}" }
+proc3 = ->(arg1, arg2) { puts "In proc3 with #{arg1} and #{arg2}" }
+
+proc1.call "ant"
+proc2.call "bee", "cat"
+proc3.call "dog", "elk"
