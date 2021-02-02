17306ca9335c1652d4ecf60b494ce91f6c4b857a
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-01 20:17:58 -0600

    Create a custom Enumerable class

---
+++ containers-blocks-and-iterators/rainbow.rb
@@ -0,0 +1,15 @@
+class Rainbow
+  include Enumerable
+  def each
+    yield 'red'
+    yield 'orange'
+    yield 'yellow'
+    yield 'green'
+    yield 'blue'
+    yield 'indigo'
+    yield 'violet'
+  end
+end
+
+r = Rainbow.new
+r.each { |color| puts "Next color: #{color}" }
