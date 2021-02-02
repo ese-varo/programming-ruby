67eec3edf268b711ce21b96d6272c6ed31ecddc2
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-02 11:11:30 -0600

    Implement conditions using blocks

--- containers-blocks-and-iterators/blocks_as_closures.rb
+++ containers-blocks-and-iterators/blocks_as_closures.rb
@@ -28,3 +28,30 @@ proc3 = ->(arg1, arg2) { puts "In proc3 with #{arg1} and #{arg2}" }
 proc1.call "ant"
 proc2.call "bee", "cat"
 proc3.call "dog", "elk"
+
+# pass one or more proc objects to a method
+def my_if(condition, then_clause, else_clause)
+  if condition
+    then_clause.call
+  else
+    else_clause.call
+  end
+end
+
+5.times do |val|
+  my_if val < 2,
+        -> { puts "#{val} is small" },
+        -> { puts "#{val} is big" }
+end
+
+def my_while(cond, &body)
+  while cond.call
+    body.call
+  end
+end
+a = 0
+
+my_while -> { a < 3 } do
+  puts a
+  a += 1
+end
